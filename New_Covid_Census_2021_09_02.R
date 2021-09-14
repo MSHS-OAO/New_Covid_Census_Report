86de798304ce600436b15bb11005f01f4cec6035
Roya Aghaeifar <71782956+Royaaghaeifar@users.noreply.github.com>
2021-09-14 08:32:18 -0400

    Add files via upload

--- New_Covid_Census_2021_09_02.R
+++ New_Covid_Census_2021_09_02.R
@@ -1,6 +1,6 @@
 
 # Code for analyzing and summarizing unit level census and COVID-19 census data
-# Last Updated 2021_09_02
+# Last Updated 2021_09_08
 
 rm(list=ls())
 
@@ -12,19 +12,28 @@ rm(list=ls())
 # Import Libraries
 library(tidyverse)
 library(readxl)
+library(writexl)
 library(reshape2)
+library(svDialogs)
+library(formattable)
+library(scales)
+library(ggpubr)
+library(knitr)
+library(kableExtra)
+library(rmarkdown)
 
 
 # Set working directory
-#Census_path <- "J:/deans/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/System Operations/Covid IP Staffing Model/Data/Epic Census Data"
-Census_path <- "C:/Users/aghaer01/Downloads/Covid"
+Census_path <- "J:/deans/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/System Operations/Covid IP Staffing Model/Data/Epic Census Data"
 setwd(Census_path)
 
 
 #Import the latest REPO file 
-repo <- file.info(list.files("../REPO", full.names = T))
+repo <- file.info(list.files(path = paste0(Census_path,"/REPO"), full.names = T))
 repo_file <- rownames(repo)[which.max(repo$mtime)]
 repo <- read_excel(repo_file)
+
+repo <- read_excel(paste0(Census_path,"/REPO/Census and Covid Repo 2020-03-12 to 2021-09-11 Created 2021-09-13 Add MSBI.xlsx"))
 
 
 # Import the most recent census data
@@ -32,11 +41,11 @@ census = file.info(list.files(path= Census_path, full.names=TRUE, pattern="ADT_B
 census = census[with(census, order(as.POSIXct(mtime), decreasing = TRUE)), ]
 
 
-if (max(repo$CensusDate)==Sys.Date()-1){
+if (max(repo$CensusDate)==Sys.Date()-2){
   files = rownames(census)[1]
   todays_census_data <-  read_xlsx(files)
 } else {
-  dif_time= difftime(max(repo$CensusDate), Sys.Date())
+  dif_time= difftime(max(repo$CensusDate), Sys.Date()-1)
   count = gsub('.*-([0-9]+).*','\\1', dif_time )
   files = rownames(census)[1:count]
   todays_census_raw <- lapply(files, function(filename){
@@ -53,11 +62,11 @@ covid = file.info(list.files(path= Census_path, full.names=TRUE, pattern="COVID 
 covid = covid[with(covid, order(as.POSIXct(mtime), decreasing = TRUE)), ]
 
 
-if (max(repo$CensusDate)==Sys.Date()-1){
+if (max(repo$CensusDate)==Sys.Date()-2){
   files = rownames(covid)[1]
   todays_covid_data <-  read_xlsx(files)
 } else {
-  dif_time= difftime(max(repo$CensusDate), Sys.Date())
+  dif_time= difftime(max(repo$CensusDate), Sys.Date()-1)
   count = gsub('.*-([0-9]+).*','\\1', dif_time )
   files = rownames(covid)[1:count]
   todays_covid_raw <- lapply(files, function(filename){
@@ -81,7 +90,6 @@ unit_mapping <- read_excel("AnalysisReference 2021-07-20.xlsx", sheet = "EpicPre
 todays_census_data <- todays_census_data %>% mutate(CensusYear= substr(YEAR_MONTH, 1, 4),
                                                     CensusMonth = substr(YEAR_MONTH, 5,6 ))
  
-
 todays_census_data <- todays_census_data %>% 
   mutate(CensusDate= as.Date(paste0(CensusMonth, "/", DAY_OF_MONTH,"/", CensusYear), format = "%m/%d/%Y"))
 
@@ -116,11 +124,11 @@ todays_covid_data <- todays_covid_data %>% mutate(CensusDate = as.Date(`REPORT R
 
 # Map site and Premier unit
 todays_covid_data <- left_join(todays_covid_data, site_mapping,  by = c("LOC_NAME" = "LOC_NAME"))
-rm(site_mapping)
 
 
 # Check data to see if there is data from an extra date
 table(todays_covid_data$CensusDate)
+
 todays_covid_data <- todays_covid_data %>%  filter(CensusDate != Sys.Date())
 
 # Remove any duplicates lines from COVID data
@@ -143,11 +151,19 @@ todays_covid_summary <- todays_covid_data %>%
   summarize(TotalPatients = n()) %>%   ungroup()
 
 
+
 # reshape data
 todays_covid_cast <- dcast(todays_covid_summary,
                            Site + LOC_NAME + DEPARTMENT_NAME + ICU + CensusDate ~
                              INFECTION_STATUS, 
                            value.var = "TotalPatients")
+
+
+# Replace NA with 0
+todays_covid_cast[is.na(todays_covid_cast$`COVID-19`), 'COVID-19'] <- 0
+todays_covid_cast[is.na(todays_covid_cast$`PUI - COVID`), 'PUI - COVID'] <- 0
+todays_covid_cast[is.na(todays_covid_cast$`PUM - RESP`), 'PUM - RESP'] <- 0
+todays_covid_cast[is.na(todays_covid_cast$`SUSC COVID`), 'SUSC COVID'] <- 0
 
 # rename columns
 colnames(todays_covid_cast)[colnames(todays_covid_cast) == "COVID-19"] <- "COVID19"
@@ -190,5 +206,78 @@ todays_census_covid_merge$PremierMapping <-
 
 
 
+# Determine if unit is a virtual unit added as part of surge planning
+todays_census_covid_merge$VirtualUnit <-
+  str_detect(todays_census_covid_merge$DEPARTMENT_NAME,
+             paste(c("VIRTUAL", "\\sVU[0-9]+", "\\sVU\\s[A-Z]"),
+                   collapse = "|"))
 
-                              
+
+# Remove any data with today's census data to ensure no duplicates
+census_date <- unique(todays_census_covid_merge$CensusDate)
+repo <- repo %>%  filter(CensusDate != census_date )
+
+
+# Bind today's data with repository
+new_repo <- rbind(repo, todays_census_covid_merge)
+new_repo <- new_repo %>% mutate(COVIDUtilization= round(COVIDUtilization, digits = 3))
+new_repo <- unique(new_repo)
+
+
+new_repo <- new_repo %>% filter(!is.na(Site))
+  
+# Save the new repo
+start_date <- min(new_repo$CensusDate)
+end_date <- max(new_repo$CensusDate)
+
+#write_xlsx(new_repo, path = paste0(Census_path, "\\REPO\\Census and Covid Repo ", start_date, " to ", end_date, " Created ", Sys.Date(), " Add MSBI.xlsx"))
+
+
+
+# Rename deactivated units before running markdown
+new_repo <- new_repo %>% mutate(DEPARTMENT_NAME = ifelse(is.na(str_extract(DEPARTMENT_NAME,
+                                                    "(?<=X_).+(?=_DEACTIVATED)")), DEPARTMENT_NAME,
+                                  str_extract(DEPARTMENT_NAME,
+                                              "(?<=X_).+(?=_DEACTIVATED)")))
+
+
+
+
+
+### Pull in MSSN Data
+setwd("../MSSN COVID")
+mssn_path <- getwd()
+
+mssn <- file.info(list.files(path = mssn_path , full.names = T, pattern="Patient Details"))
+mssn_file <- rownames(mssn)[which.max(mssn$mtime)]
+raw_data_mssn <- read_excel(mssn_file)
+
+# read mapping mssn
+unit_mapping_mssn_covid  <- read_excel("MSSN Unit Mapping 12-15-2020.xlsx", col_names = TRUE, na = c("", "NA"))
+
+setwd(Census_path)
+setwd("../..")
+
+setwd("C:\\Users\\aghaer01\\Downloads")
+
+save_output <- paste0(getwd(), "\\Daily Reporting Output")
+# setwd("J:\\Presidents\\HSPI-PM\\Operations Analytics and Optimization\\Projects\\System Operations\\Covid IP Staffing Model\\Code")
+rmarkdown::render("Code\\Epic Unit and COVID Census Analysis REPORT 2020-12-15_Inc_MSSN_New_Report_v2.Rmd", output_file = paste("Epic Census and Covid Daily Reporting", Sys.Date()), output_dir = save_output)
+
+
+
+
+### Markdown code:
+start_date <-  format(start_date, "%m-%d-%Y")
+end_date   <- format(end_date, "%m-%d-%Y")
+
+report_run_date <- Sys.time()
+raw_data <- new_repo %>% mutate(non-COVID19 = OCCUPIED_COUNT - COVID19 - SUSC - PUI - PUM,
+                                `Open Beds` = TOTAL_BED_CENSUS - OCCUPIED_COUNT)
+
+
+raw_data <- raw_data %>% mutate(`Unit Type` = ifelse(UnitType== "CC"|UnitType== "NCC", "Adult Med Surg", UnitType ))
+raw_data <- raw_data %>% mutate(`Unit Type High` = ifelse(UnitType== "ED" , "ED", "IP" ))
+
+
+
